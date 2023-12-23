6c0df90c8b0ac2332028bf1793b34b87d4374a96
R.Ito <iriku.ir@gmail.com>
2023-12-23 15:26:46 +0900

    impl forground notification

--- TestApp/AppDelegate.swift
+++ TestApp/AppDelegate.swift
@@ -8,9 +8,16 @@
 import Foundation
 import UIKit
 
+
 class AppDelegate:NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    // NotificationDelegateクラスのインスタンス生成
+    let notificationDelegate = NotificationDelegate()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-      let center = UNUserNotificationCenter.current()
+        //UNUserNotificationCenterのデリゲートにnotificationDelegateを設定
+        UNUserNotificationCenter.current().delegate = notificationDelegate
+        
+        let center = UNUserNotificationCenter.current()
         center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
             if granted {
                 print("許可されました！")
@@ -20,11 +27,13 @@ class AppDelegate:NSObject, UIApplicationDelegate, UNUserNotificationCenterDeleg
         }
         return true
     }
-    
-    func userNotificationCenter(
-        _ center: UNUserNotificationCenter,
-        willPresent notification: UNNotification,
-        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-            completionHandler([[.banner, .list, .sound]])
+}
+
+class NotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // フォアグラウンドでの通知表示オプション
+        completionHandler([.banner, .sound])
     }
 }
